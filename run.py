b3926cfa2eb8b8ff02d4b51c8c63988f8a416ed9
Mark Nevison <mnevison2@gmail.com>
2024-09-13 08:42:15 +0000

    updates surplus func

--- run.py
+++ run.py
@@ -76,8 +76,14 @@ def calc_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
 
+
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 
 
@@ -88,7 +94,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calc_surplus_data(sales_data)
+    new_surplus_data = calc_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
 print("Welcome to Love Sandwiches Data Automation\n")
